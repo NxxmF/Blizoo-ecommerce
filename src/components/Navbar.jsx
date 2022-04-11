87e0c5695efe2d57eff77a168bcfa91edbb2c2bb
Naim Farhat <naimxfarhat21@gmail.com>
2022-04-11 18:34:39 +0800

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>Blizoo.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
